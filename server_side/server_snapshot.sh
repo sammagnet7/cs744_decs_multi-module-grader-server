4ad8e6fdb7303865542e825c67984b018255ee7e
Soumik Dutta <sammagnet7@gmail.com>
2023-11-30 05:10:39 +0530

    Fixed issue

--- server_side/server_snapshot.sh
+++ server_side/server_snapshot.sh
@@ -1,129 +1,38 @@
-#!/usr/bin/python3
-### Thi script is used to analyse and plot dependng upon server side logged data
+#!/bin/bash
 
-import matplotlib.pyplot as plt
-from subprocess import run, PIPE
-import os
-
-plt.interactive(False)
-
-#Received file name
-
-server_snapshot_log = os.path.expanduser('~/Documents/server_snapshot.log')
-server_snapshot_nc_log = os.path.expanduser('~/Documents/server_snapshot_nc.log')
-server_service_time_log = os.path.expanduser('~/Documents/serviceTime.log')
-
-# Create lists to store the data
-clients = []
-averageCpuUtilization = []
-averageNumberOfThreads = []
-averageServiceTime = []
-
-with open(server_snapshot_log, 'r') as log_file, open(server_snapshot_nc_log, 'r') as nc_file,open(server_service_time_log,'r') as service_time_log:
-    log_lines = log_file.readlines()
-    nc_lines = nc_file.readlines()
-    service_time_line = service_time_log.readlines()
-
-    log_lines = [line.strip() for line in log_lines]
-    nc_lines = [line.strip() for line in nc_lines]
-    service_time_line = [line.strip() for line in service_time_line]
+client_machine_path="ankur@10.96.20.80:~/Documents"
 
 
-    log_line_iterator=0
-    service_time_line_iterator = 0
-    
-    #iteration in netcat file
-    for n in nc_lines:
-        if n.startswith("Number of clients"):
-            client_parts = n.split(":")
-            clients.append(int(client_parts[1].strip()))
-            
-            cnt_log_line=0
-            cnt_service_time_line=0
-            
-            avg_cpu=0.0
-            avg_thread=0.0
-            tot_service_time=0.0
-            
-            #iteration in log file
-            while log_line_iterator<len(log_lines):
-                parts = log_lines[log_line_iterator].split()
-                # Extract the three parts
-                timestamp = parts[0]
-                thread = parts[1]
-                cpu_utilization = parts[2]
+mkdir -p temp_files
 
-                if timestamp<=prev:
-                    avg_thread+=int(thread.strip())
-                    avg_cpu+=float(cpu_utilization.strip())
-                    cnt_log_line+=1
-                else:
-                    break
-                # print(timestamp,thread,cpu_utilization)
-                log_line_iterator+=1
+log_file="temp_files/server_snapshot.log"
+nc_output_file="temp_files/server_snapshot_nc.log"
+serviceTime_file="temp_files/serviceTime.log"
 
-            #iteration in averageQ_log file
-            while service_time_line_iterator<len(service_time_line):
-                parts = service_time_line[service_time_line_iterator].split()
-                # Extract the three parts
-                timestamp = parts[0]
-                service_time = parts[1]
- 
-                if timestamp<=prev:
-                    tot_service_time +=int(service_time.strip())
-                    cnt_service_time_line+=1
-                else:
-                    break
-                # print(timestamp,thread,cpu_utilization)
-                service_time_line_iterator+=1
-                
-                
-            averageCpuUtilization.append(avg_cpu/cnt_log_line)
-            averageNumberOfThreads.append(avg_thread/cnt_log_line)
-            averageServiceTime.append(0 if cnt_service_time_line == 0 else tot_service_time / cnt_service_time_line)
- 
+while [ true ]; do
+    COND=$(ps aux | grep "server 8080" | wc -l)
+    if [ $COND -gt 1 ]; then
+        # Get the current timestamp
+        timestamp=$(date "+%H:%M:%S")
 
-        elif n=="exit":
-            break
-        else:
-            prev=n    
+        # Start listening on port 12345 and append to the nc output file with timestamp
+        { echo "$timestamp"; nc -l -p 12345; } >> "$nc_output_file" &
+        # Get active threads and CPU utilization with a timestamp and append to the same line in the log file
+        threads=$(ps -eLf | grep "./server 8080" | awk 'NR==1{print $6}')
+        cpu_utilization=$(top -b -n 1 | awk -F',' '/%Cpu/ {print $1}' | sed 's/[^0-9.]//g')
+        
+        if [ "$cpu_utilization" != "0.0" ]; then
+            echo "threads :" $threads " cpu Utilization: " $cpu_utilization
+            echo "$timestamp $threads $cpu_utilization" >> "$log_file"
+        fi
 
-    
-# Now you can use the collected data to plot the graphs
-print("averageCpuUtilization:", averageCpuUtilization)
-print("averageNumberOfThreads:", averageNumberOfThreads)
-print("averageServiceTimes:",averageServiceTime)
-
-
-###################
-#Plotting graph:#
-###################
-
-fig, axs = plt.subplots(2, 2)
-fig.suptitle('Autograding server performance analysis : Server side')
- 
- 
-axs[0,0].plot(clients, averageNumberOfThreads, color='magenta', marker='o', markersize=2)
-axs[0,0].set(xlabel='Number of clients',ylabel='Thread counts (as an average)')
-axs[0,0].set_title("Number of Clients vs Average Thread counts")
- 
-axs[0,1].plot(clients, averageCpuUtilization, color='red', marker='o', markersize=2)
-axs[0,1].set(xlabel='Number of clients', ylabel='Cpu Utilization (in %)')
-axs[0,1].set_title("Number of Clients vs Cpu Utilization")
- 
-axs[1,0].plot(clients, averageServiceTime, color='blue', marker='o', markersize=2)
-axs[1,0].set(xlabel='Number of clients', ylabel='Service time (in ms as an average)')
-axs[1,0].set_title("Number of Clients vs Average Service time (in ms)")
- 
-axs[0,0].grid(color = 'green', linestyle = '--', linewidth = 0.5)
-axs[0,1].grid(color = 'green', linestyle = '--', linewidth = 0.5)
-axs[1,0].grid(color = 'green', linestyle = '--', linewidth = 0.5)
-axs[1,1].set_visible(False)
- 
-fig.tight_layout()
- 
-plt.show()
-
-os.remove(server_snapshot_log)
-os.remove(server_snapshot_nc_log)
-os.remove(server_service_time_log)
+        sleep 3
+    else
+    	echo "Starting transferring the log files to the client machine....."
+        # Copy log.txt to a remote location using scp
+        scp "$log_file" "$nc_output_file" "$serviceTime_file" $client_machine_path
+        pkill -f "nc -l -p 12345"
+        rm -rf temp_files
+        break
+    fi
+done
